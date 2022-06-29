06fa4d4c6479715a61681eaac49a2d6fa73f1d54
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-06-29 00:08:10 -0600

    feat: add base strc

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,19 @@ import {
   Grid,
   Container,
 } from "@material-ui/core";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
 
 function App() {
   return (
-    <div className="App">
-      <Typography variant="h1">Text</Typography>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 }
 
